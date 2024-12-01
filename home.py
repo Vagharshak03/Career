a50c529d74e26d8d5ed4cb727540011cb5834d1d
vagharshak003 <vagharshakavetisyan03@gmail.com>
2024-12-01 14:38:40 +0400

    small changes

--- home.py
+++ home.py
@@ -1,24 +1,22 @@
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
-@app.route('/register')
-def register():
-    requestapp = request.args
-    print(requestapp)
-    return render_template("register.html")
-
-@app.route('/login')
-def login():
-    return render_template("login.html")
-
-@app.route('/subscribe')
-def login():
-    return render_template("checkout.html")
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
+@app.route('/subscribe')
+def subscribe():
+    return render_template("checkout.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(port='8000', debug=True)
